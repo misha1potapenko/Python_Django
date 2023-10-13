680e9b184d8b7db63751898625a6a5ca4af7813b
misha1potapenko <misha1potapenko@yandex.ru>
2023-10-13 21:10:41 +0300

    added two files html

--- first_project/firstapp/views.py
+++ first_project/firstapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+import logging
 
 # Create your views here.
 from django.http import HttpResponse
 
+logger = logging.getLogger(__name__)
 
 def index(request):
-    return HttpResponse("Hello, world!")
-
+    logger.info('Index page accessed')
+    return render(request, 'main.html')
 
 def about(request):
-    return HttpResponse("About us")
+    logger.info('Index page accessed')
+    return render(request, 'about.html')
