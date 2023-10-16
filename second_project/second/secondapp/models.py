89b74e643572a23fdfd8ca6a225e9054e3cc9fc1
misha1potapenko <misha1potapenko@yandex.ru>
2023-10-16 17:29:49 +0300

    added def __str__

--- second_project/second/secondapp/models.py
+++ second_project/second/secondapp/models.py
@@ -40,6 +40,8 @@ class User(models.Model):
     age = models.IntegerField()
     date_registrations = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'name: {self.name}, email: {self.email}'
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -48,9 +50,15 @@ class Product(models.Model):
     count_product = models.DecimalField(max_digits=30, decimal_places=3)
     date_add = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'name: {self.name}, price: {self.price}, description: {self.description}'
+
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date_ordered = models.DateTimeField(auto_now_add=True)
     all_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'customer: {self.customer}, product: {self.products}, price: {self.all_price}'
