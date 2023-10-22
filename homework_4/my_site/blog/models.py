507294fa4f63fc21bbb82507ee00e1b8e1a63aec
misha1potapenko <misha1potapenko@yandex.ru>
2023-10-22 13:13:09 +0300

    added class User Product Order

--- homework_4/my_site/blog/models.py
+++ homework_4/my_site/blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
+    address = models.CharField(max_length=150)
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+    date_registrations = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'name: {self.name}, email: {self.email}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    count_product = models.DecimalField(max_digits=30, decimal_places=3)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'name: {self.name}, price: {self.price}, description: {self.description}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    all_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        ordering = ['customer']
+
+    def __str__(self):
+        return f'customer: {self.customer}, product: {self.products}, price: {self.all_price}'
--- homework_4/my_site/my_site/settings.py
+++ homework_4/my_site/my_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "my_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,3 +122,22 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {'version': 1,
+           'disable_existing_loggers': False,
+           'formatters': {
+               'verbose': {
+                   'format': '{levelname} {asctime} {module} {process} {thread} {message}',
+                   'style': '{',
+               },
+               'simple': {
+                   'format': '%(levelname)s %(message)s'},
+           },
+           'handlers': {
+               'console': {'class': 'logging.StreamHandler', 'formatter': 'verbose', },
+               'file': {'class': 'logging.FileHandler', 'filename': 'django.log', 'formatter': 'verbose', },
+           },
+           'loggers': {'django': {'handlers': ['console', 'file'], 'level': 'INFO', },
+                       'myapp': {'handlers': ['console', 'file'], 'level': 'DEBUG', 'propagate': True, }, }, }
+
+
